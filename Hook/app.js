94a4329aadbfcf7afb732e50641c812bdc5e6011
Zhongcai Ng <ngzhongcai@hotmail.com>
2016-01-16 15:20:01 +0800

    Cloudilly / DingDong
    Cloudilly / DingDong

--- Hook/app.js
+++ Hook/app.js
@@ -11,7 +11,7 @@ cloudilly.socketConnected(function(err, res) {
 	console.log("@@@@@@ CONNECTED");
 	console.log(res);
 
-	var beaconID= "<INSERT YOUR OWN UNIQUE BEACON ID, FORMAT AS dingdong:major:minor>";
+	var beaconID= "<INSERT YOUR OWN UNIQUE BEACON ID, FORMAT AS 'DINGDONG:MAJOR:MINOR', MAJOR / MINOR FROM ESTIMOTE CLOUD>";
 	var secretToken= "<INSERT YOUR OWN SECRET TOKEN>";
 	cloudilly.create(beaconID, secretToken, function(err, res) {
 		if(err) { console.log("ERROR: Oops. Something wrong"); return; }
